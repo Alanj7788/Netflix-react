14984f490e0a90ff18d6a4866d1db5c6fa3eec75
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2024-02-18 12:06:55 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import './App.css'
 import Banner from './components/Banner/Banner'
+import RowPost from './components/RowPost/RowPost'
+import axios  from 'axios'
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner/>
+      <RowPost/>
     </div>
   )
 }
