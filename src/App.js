4a6820f5e597c950147ad99b853b3726aca9930b
ALAN JOSE <130158432+Alanj7788@users.noreply.github.com>
2024-02-20 00:28:27 +0530

    trending,action,originals added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import './App.css'
 import Banner from './components/Banner/Banner'
 import RowPost from './components/RowPost/RowPost'
 import axios  from 'axios'
+import { action, originals, trending } from './urls'
 function App() {
   return (
     <div>
       <Navbar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={trending} title='Trending' isSmall />
+
     </div>
   )
 }
